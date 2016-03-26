9e73f153347de6a0b4da5fba580ee9fdd07dd74b
Eugene DePrince <eugene.deprince@gmail.com>
2016-03-26 16:00:26 -0400

    write tpdm using struct so the code is a little cleaner

--- write_tpdm.cc
+++ write_tpdm.cc
@@ -37,6 +37,13 @@ using namespace psi;
 
 namespace psi{namespace v2rdm_casscf{
 
+struct tpdm {
+    int i;
+    int j;
+    int k;
+    int l;
+    double val;
+};
 
 
 void v2RDMSolver::WriteTPDM(){
@@ -71,11 +78,13 @@ void v2RDMSolver::WriteTPDM(){
 
                 double valab = x_p[d2aboff[h] + ij*gems_ab[h] + kl];
 
-                psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&ifull,sizeof(int),addr_ab,&addr_ab);
-                psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&jfull,sizeof(int),addr_ab,&addr_ab);
-                psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&kfull,sizeof(int),addr_ab,&addr_ab);
-                psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&lfull,sizeof(int),addr_ab,&addr_ab);
-                psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&valab,sizeof(double),addr_ab,&addr_ab);
+                tpdm d2;
+                d2.i   = ifull;
+                d2.j   = jfull;
+                d2.k   = kfull;
+                d2.l   = lfull;
+                d2.val = valab;
+                psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&d2,sizeof(tpdm),addr_ab,&addr_ab);
 
                 if ( i != j && k != l ) {
 
@@ -88,21 +97,17 @@ void v2RDMSolver::WriteTPDM(){
                     double valaa = sij * skl * x_p[d2aaoff[h] + ija*gems_aa[h] + kla];
                     double valbb = sij * skl * x_p[d2bboff[h] + ija*gems_aa[h] + kla];
 
-                    psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&ifull,sizeof(int),addr_aa,&addr_aa);
-                    psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&jfull,sizeof(int),addr_aa,&addr_aa);
-                    psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&kfull,sizeof(int),addr_aa,&addr_aa);
-                    psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&lfull,sizeof(int),addr_aa,&addr_aa);
-                    psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&valaa,sizeof(double),addr_aa,&addr_aa);
+                    d2.val = valaa;
+                    psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&d2,sizeof(tpdm),addr_aa,&addr_aa);
 
-                    psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&ifull,sizeof(int),addr_bb,&addr_bb);
-                    psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&jfull,sizeof(int),addr_bb,&addr_bb);
-                    psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&kfull,sizeof(int),addr_bb,&addr_bb);
-                    psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&lfull,sizeof(int),addr_bb,&addr_bb);
-                    psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&valbb,sizeof(double),addr_bb,&addr_bb);
+                    d2.val = valbb;
+                    psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&d2,sizeof(tpdm),addr_bb,&addr_bb);
+
                 }
             }
         }
     }
+
     // core-core
     for (int hi = 0; hi < nirrep_; hi++) {
 
@@ -116,42 +121,27 @@ void v2RDMSolver::WriteTPDM(){
 
                     int jfull      = j + pitzer_offset_full[hj];
 
-                    double val = 1.0;
-
-                    psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&ifull,sizeof(int),addr_ab,&addr_ab);
-                    psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&jfull,sizeof(int),addr_ab,&addr_ab);
-                    psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&ifull,sizeof(int),addr_ab,&addr_ab);
-                    psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&jfull,sizeof(int),addr_ab,&addr_ab);
-                    psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&val,sizeof(double),addr_ab,&addr_ab);
+                    tpdm d2;
+                    d2.i   = ifull;
+                    d2.j   = jfull;
+                    d2.k   = ifull;
+                    d2.l   = jfull;
+                    d2.val = 1.0;
+                    psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&d2,sizeof(tpdm),addr_ab,&addr_ab);
 
                     if ( i != j ) {
 
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&ifull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&jfull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&ifull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&jfull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&val,sizeof(double),addr_aa,&addr_aa);
-
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&ifull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&jfull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&ifull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&jfull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&val,sizeof(double),addr_bb,&addr_bb);
+                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&d2,sizeof(tpdm),addr_aa,&addr_aa);
+                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&d2,sizeof(tpdm),addr_bb,&addr_bb);
 
                         // ij;ji
-                        val = -1.0;
 
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&ifull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&jfull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&jfull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&ifull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&val,sizeof(double),addr_aa,&addr_aa);
+                        d2.val = -1.0;
+                        d2.k   = jfull;
+                        d2.l   = ifull;
 
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&ifull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&jfull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&jfull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&ifull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&val,sizeof(double),addr_bb,&addr_bb);
+                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&d2,sizeof(tpdm),addr_aa,&addr_aa);
+                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&d2,sizeof(tpdm),addr_bb,&addr_bb);
 
                     }
                 }
@@ -181,73 +171,70 @@ void v2RDMSolver::WriteTPDM(){
                         double valaa = x_p[d1aoff[hj]+j*amopi_[hj]+l];
                         double valbb = x_p[d1boff[hj]+j*amopi_[hj]+l];
 
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&ifull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&jfull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&ifull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&lfull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&valaa,sizeof(double),addr_aa,&addr_aa);
+                        tpdm d2;
 
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&jfull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&ifull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&lfull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&ifull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&valaa,sizeof(double),addr_aa,&addr_aa);
+                        // ij;il
+                        d2.i   = ifull;
+                        d2.j   = jfull;
+                        d2.k   = ifull;
+                        d2.l   = lfull;
 
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&ifull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&jfull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&ifull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&lfull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&valbb,sizeof(double),addr_bb,&addr_bb);
+                        d2.val = valaa;
+                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&d2,sizeof(tpdm),addr_aa,&addr_aa);
 
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&jfull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&ifull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&lfull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&ifull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&valbb,sizeof(double),addr_bb,&addr_bb);
+                        d2.val = valbb;
+                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&d2,sizeof(tpdm),addr_bb,&addr_bb);
 
-                        // aa/bb with minus sign: ij;li, ji;il
-                        valaa = -valaa;
-                        valbb = -valbb;
+                        // ij;li
+                        d2.k   = lfull;
+                        d2.l   = ifull;
 
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&ifull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&jfull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&lfull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&ifull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&valaa,sizeof(double),addr_aa,&addr_aa);
+                        d2.val = -valaa;
+                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&d2,sizeof(tpdm),addr_aa,&addr_aa);
 
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&jfull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&ifull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&ifull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&lfull,sizeof(int),addr_aa,&addr_aa);
-                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&valaa,sizeof(double),addr_aa,&addr_aa);
+                        d2.val = -valbb;
+                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&d2,sizeof(tpdm),addr_bb,&addr_bb);
 
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&ifull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&jfull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&lfull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&ifull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&valbb,sizeof(double),addr_bb,&addr_bb);
+                        // ji;li
+                        d2.i   = jfull;
+                        d2.j   = ifull;
 
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&jfull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&ifull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&ifull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&lfull,sizeof(int),addr_bb,&addr_bb);
-                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&valbb,sizeof(double),addr_bb,&addr_bb);
+                        d2.val = valaa;
+                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&d2,sizeof(tpdm),addr_aa,&addr_aa);
+
+                        d2.val = valbb;
+                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&d2,sizeof(tpdm),addr_bb,&addr_bb);
+
+                        // ji;il
+                        d2.k   = ifull;
+                        d2.l   = lfull;
+
+                        d2.val = -valaa;
+                        psio->write(PSIF_V2RDM_D2AA,"D2aa",(char*)&d2,sizeof(tpdm),addr_aa,&addr_aa);
+
+                        d2.val = -valbb;
+                        psio->write(PSIF_V2RDM_D2BB,"D2bb",(char*)&d2,sizeof(tpdm),addr_bb,&addr_bb);
+
 
                         // ab (ij;il) and ba (ji;li) pieces
                         double valab = x_p[d1boff[hj]+j*amopi_[hj]+l];
                         double valba = x_p[d1aoff[hj]+j*amopi_[hj]+l];
 
-                        psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&ifull,sizeof(int),addr_ab,&addr_ab);
-                        psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&jfull,sizeof(int),addr_ab,&addr_ab);
-                        psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&ifull,sizeof(int),addr_ab,&addr_ab);
-                        psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&lfull,sizeof(int),addr_ab,&addr_ab);
-                        psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&valab,sizeof(double),addr_ab,&addr_ab);
+                        // ij;il
+                        d2.i   = ifull;
+                        d2.j   = jfull;
 
-                        psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&jfull,sizeof(int),addr_ab,&addr_ab);
-                        psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&ifull,sizeof(int),addr_ab,&addr_ab);
-                        psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&lfull,sizeof(int),addr_ab,&addr_ab);
-                        psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&ifull,sizeof(int),addr_ab,&addr_ab);
-                        psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&valba,sizeof(double),addr_ab,&addr_ab);
+                        d2.val = valab;
+                        psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&d2,sizeof(tpdm),addr_ab,&addr_ab);
+
+                        // ji;li
+                        d2.i   = jfull;
+                        d2.j   = ifull;
+                        d2.k   = lfull;
+                        d2.l   = ifull;
+
+                        d2.val = valba;
+                        psio->write(PSIF_V2RDM_D2AB,"D2ab",(char*)&d2,sizeof(tpdm),addr_ab,&addr_ab);
 
                     }
                 }
@@ -260,6 +247,72 @@ void v2RDMSolver::WriteTPDM(){
     psio->close(PSIF_V2RDM_D2BB,1);
     psio->close(PSIF_V2RDM_D2AB,1);
 }
+
+/*void v2RDMSolver::ReadTPDM(){
+
+    int full = nmo + nfrzc;
+
+    double * D2aa = (double*)malloc(full*full*full*full*sizeof(double));
+    double * D2bb = (double*)malloc(full*full*full*full*sizeof(double));
+    double * D2ab = (double*)malloc(full*full*full*full*sizeof(double));
+
+    memset((void*)D2aa,'\0',full*full*full*full*sizeof(double));
+    memset((void*)D2bb,'\0',full*full*full*full*sizeof(double));
+    memset((void*)D2ab,'\0',full*full*full*full*sizeof(double));
+
+    boost::shared_ptr<PSIO> psio (new PSIO());
+
+    psio->open(PSIF_V2RDM_D2AA,PSIO_OPEN_OLD);
+    psio->open(PSIF_V2RDM_D2BB,PSIO_OPEN_OLD);
+    psio->open(PSIF_V2RDM_D2AB,PSIO_OPEN_OLD);
+
+    psio_address addr_aa = PSIO_ZERO;
+    psio_address addr_bb = PSIO_ZERO;
+    psio_address addr_ab = PSIO_ZERO;
+
+    psio_address end_aa = psio_get_entry_end(PSIF_V2RDM_D2AA,"D2aa");
+    psio_address end_bb = psio_get_entry_end(PSIF_V2RDM_D2BB,"D2bb");
+    psio_address end_ab = psio_get_entry_end(PSIF_V2RDM_D2AB,"D2ab");
+
+    do { 
+        int i,j,k,l;
+        double val;
+        psio->read(PSIF_V2RDM_D2AB,"D2ab",(char*)&i,sizeof(int),addr_ab,&addr_ab);
+        psio->read(PSIF_V2RDM_D2AB,"D2ab",(char*)&j,sizeof(int),addr_ab,&addr_ab);
+        psio->read(PSIF_V2RDM_D2AB,"D2ab",(char*)&k,sizeof(int),addr_ab,&addr_ab);
+        psio->read(PSIF_V2RDM_D2AB,"D2ab",(char*)&l,sizeof(int),addr_ab,&addr_ab);
+        psio->read(PSIF_V2RDM_D2AB,"D2ab",(char*)&val,sizeof(double),addr_ab,&addr_ab);
+        long int id = i*full*full*full+j*full*full+k*full+l;
+        D2ab[id] = val;
+    }while(addr_ab != end_ab);
+    do { 
+        int i,j,k,l;
+        double val;
+        psio->read(PSIF_V2RDM_D2AA,"D2aa",(char*)&i,sizeof(int),addr_aa,&addr_aa);
+        psio->read(PSIF_V2RDM_D2AA,"D2aa",(char*)&j,sizeof(int),addr_aa,&addr_aa);
+        psio->read(PSIF_V2RDM_D2AA,"D2aa",(char*)&k,sizeof(int),addr_aa,&addr_aa);
+        psio->read(PSIF_V2RDM_D2AA,"D2aa",(char*)&l,sizeof(int),addr_aa,&addr_aa);
+        psio->read(PSIF_V2RDM_D2AA,"D2aa",(char*)&val,sizeof(double),addr_aa,&addr_aa);
+        long int id = i*full*full*full+j*full*full+k*full+l;
+        D2aa[id] = val;
+    }while(addr_aa != end_aa);
+    do { 
+        int i,j,k,l;
+        double val;
+        psio->read(PSIF_V2RDM_D2BB,"D2bb",(char*)&i,sizeof(int),addr_bb,&addr_bb);
+        psio->read(PSIF_V2RDM_D2BB,"D2bb",(char*)&j,sizeof(int),addr_bb,&addr_bb);
+        psio->read(PSIF_V2RDM_D2BB,"D2bb",(char*)&k,sizeof(int),addr_bb,&addr_bb);
+        psio->read(PSIF_V2RDM_D2BB,"D2bb",(char*)&l,sizeof(int),addr_bb,&addr_bb);
+        psio->read(PSIF_V2RDM_D2BB,"D2bb",(char*)&val,sizeof(double),addr_bb,&addr_bb);
+        long int id = i*full*full*full+j*full*full+k*full+l;
+        D2bb[id] = val;
+    }while(addr_bb != end_bb);
+
+    // close files
+    psio->close(PSIF_V2RDM_D2AA,1);
+    psio->close(PSIF_V2RDM_D2BB,1);
+    psio->close(PSIF_V2RDM_D2AB,1);
+}*/
 
 }} //end namespaces
 
